3022fe9998a74f626d81a68ee9a6ece82c29f336
Pumposh Bhat <pumposh@live.com>
2017-09-13 12:44:58 -0500

    Add eventlog directory with sample event file and update FileRead.java

--- project-files/src/converge/FileRead.java
+++ project-files/src/converge/FileRead.java
@@ -1,19 +1,35 @@
 package converge;
-import java.io.File;
+import java.io.*;
 
-public class FileRead {
-
-	
+public class FileRead 
+{	
 	private static void readEvents()
 	{
-		File folder = new File("/eventlog");
+		//Initialize input stream
+	    FileInputStream in = null;
+		
+		//Navigate to eventlog directory where events are stored as .txt files
+		File folder = new File("/eventlog/");
+		
+		//Create a list of all the files in the directory
 		File[] listOfFiles = folder.listFiles();
 
-		for (int i = 0; i < listOfFiles.length; i++) {
+		//Iterate through files
+		for (int i = 0; i < listOfFiles.length; i++) 
+		{
 		  File file = listOfFiles[i];
-		  if (file.isFile() && file.getName().endsWith(".txt")) {
-		    String content = FileUtils.readFileToString(file);
-		    /* do something with content */
+		  if (file.isFile() && file.getName().endsWith(".txt")) 
+		  {
+				//in file object within try catch block
+		        try 
+		        {
+					in = new FileInputStream(file);
+				} catch (FileNotFoundException e) 
+		        {
+					e.printStackTrace();
+				}
+		        
+		        
 		  } 
 		}
 	}
