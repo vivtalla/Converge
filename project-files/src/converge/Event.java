dcaab0c01860af0a62a734a7681ada255366b135
Vivek Tallavajhala <vivtalla@ku.edu>
2017-09-13 21:15:55 -0500

    Add Basic Methods in Event.java
    This commit begins to add necessary functions in the Event.java file.

--- project-files/src/converge/Event.java
+++ project-files/src/converge/Event.java
@@ -1,5 +1,63 @@
 package converge;
 
-public class Event {
+import java.util.Vector;
 
+public class Event {
+	String a_eventName;
+	int a_month;
+	int a_day;
+	int a_year;
+	Vector a_attendees = new Vector();
+	Vector a_adminAvailability = new Vector();
+	public Event() {}
+	public Event(String eventName, int month, int day, int year, Vector attendees, Vector adminAvailability )
+	{
+		a_eventName = eventName;
+		a_month = month;
+		a_day = day;
+		a_year = year;
+		a_attendees = attendees;
+		a_adminAvailability = adminAvailability;
+		
+	}
+	public void setEventName(String eventName)
+	{
+		a_eventName = eventName;
+	}
+	public String getEventName()
+	{
+		return a_eventName;
+	}
+	public void setMonth( int month )
+	{
+		a_month = month;
+	}
+	public int getMonth()
+	{
+		return a_month;
+	}
+	public void setDay(int day)
+	{
+		a_day = day;
+	}
+	public int getDay()
+	{
+		
+		return a_day;
+	}
+	public void setYear(int year)
+	{
+		a_year = year;
+	}
+	public int getYear()
+	{
+		return a_year;
+	}
+	
+	private static void exportEvents()
+	{
+		
+	}
+	
+	
 }
