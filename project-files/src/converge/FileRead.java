87d9e34b83503089dfda06e0ad13a6dfc4c8a1c6
Pumposh Bhat <pumposh@live.com>
2017-09-13 22:43:57 -0500

    Updates FileRead

--- project-files/src/converge/FileRead.java
+++ project-files/src/converge/FileRead.java
@@ -1,18 +1,29 @@
 package converge;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FileRead 
+public class FileRead
 {	
-	private static void readEvents()
+	public static void main(String[] args) throws IOException
 	{
-		//Initialize input stream
-	    FileInputStream in = null;
+		readEvents();
+	}
+	
+	private static List<Event> readEvents() throws IOException
+	{
+		//Declare List of events
+		List<Event> events;
 		
-		//Navigate to eventlog directory where events are stored as .txt files
-		File folder = new File("/eventlog/");
+		//Declare input stream
+		FileReader in = null;
+		
+		//Navigate to event log directory where events are stored as .txt files
+		File folder = new File("event_log/");
 		
 		//Create a list of all the files in the directory
 		File[] listOfFiles = folder.listFiles();
+	  	List<String> records = new ArrayList<String>();
 
 		//Iterate through files
 		for (int i = 0; i < listOfFiles.length; i++) 
@@ -20,19 +31,35 @@ public class FileRead
 		  File file = listOfFiles[i];
 		  if (file.isFile() && file.getName().endsWith(".txt")) 
 		  {
+			  
 				//in file object within try catch block
 		        try 
 		        {
-					in = new FileInputStream(file);
-				} catch (FileNotFoundException e) 
+					in = new FileReader(file);
+				} 
+		        
+		        catch (FileNotFoundException e) 
 		        {
 					e.printStackTrace();
 				}
 		        
+		        //Take file input as multiple lines in 'record' list
+	            BufferedReader bufferedReader = new BufferedReader(in);
+		        String line;
 		        
+		        while ((line = bufferedReader.readLine()) != null)
+		        {
+		          records.add(line);
+		        }
+		        
+		        Event e;
+		        System.out.println(records.get(1));
+		        
+		        e.
 		        
 		  } 
 		}
+		System.out.println(records);
 	}
 }
 
