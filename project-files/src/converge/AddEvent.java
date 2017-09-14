2383d761f6ce62f2123d2dac0af77714a2a588cf
Vivek Tallavajhala <vivtalla@ku.edu>
2017-09-14 01:09:46 -0500

    Add Basic User Interaction to AddEvent.java
    This commit begins the basic user interaction for the Converge App.
    It asks for the basic elements needed to build an event and places them
    inside an event object.

--- project-files/src/converge/AddEvent.java
+++ project-files/src/converge/AddEvent.java
@@ -1,1 +1,28 @@
+import converge.Event;
 
+import java.util.Scanner;
+
+public class AddEvent{
+	public Event() newEvent;
+	
+	Scanner userInput = new Scanner(System.in); //allows for user input
+	
+	System.out.println("Enter the name of the event: ");
+	
+	newEvent.setEventName = userInput.nextString();
+	
+	System.out.println("Enter the Month of the event: ");
+	
+	newEvent.setMonth() = userInput.nextInt();
+	
+	System.out.println("Enter the Day of the event: ");
+	
+	newEvent.setDay() = userInput.nextInt();
+	
+	System.out.println("Enter the year of the event: ");
+	
+	newEvent.setYear() = userInput.nextYear();
+	
+	
+	
+}
