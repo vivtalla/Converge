7c9f75d629b5c305907685e35ec48dd19d5731c9
Pumposh Bhat <pumposh@live.com>
2017-09-17 23:39:38 -0500

    Update JoinEvent.java

--- project-files/src/converge/JoinEvent.java
+++ project-files/src/converge/JoinEvent.java
@@ -11,6 +11,75 @@ import java.util.Scanner;
  */
 
 public class JoinEvent {
+	
+	public static void view() throws IOException
+	{
+		FileRead fr = new FileRead();
+		Vector<Event> events = fr.readEvents();
+		Vector<Integer> times=new Vector<Integer>();
+		int eventChoice = 0;
+		Scanner myScan = new Scanner(System.in);
+		int timeChoice = 0;
+		boolean invalidInput = true;
+		clearScreen();
+		while(invalidInput) 
+		{
+			System.out.println("12 Hour / 24 Hour Mode? (12/24):");
+				timeChoice = myScan.nextInt();
+				if(timeChoice == 12 || timeChoice == 24) 
+				{
+					invalidInput = false;
+				}
+				else 
+				{
+					clearPrint("Error\nThe selection is invalid.\n");
+					invalidInput = true;
+				}
+		}
+		invalidInput = true;
+		clearScreen();
+		
+		while(invalidInput) 
+		{
+			System.out.println("Which event would you like to view?");
+			System.out.println("Please enter the number cooresponding to your event choice.\n");
+			for(int i=0; i<events.size(); i++) 
+			{
+				System.out.println((i+1)+". "+events.elementAt(i).getEventName());
+			}
+				eventChoice = myScan.nextInt();
+				if(eventChoice > events.size() || eventChoice < 1) 
+				{
+					clearPrint("Error\nThe selection is invalid.\n");
+					invalidInput = true;
+				}
+				else 
+				{
+					invalidInput = false;
+				}
+		}
+		clearScreen();
+		
+		clearPrint("The following attendees of " + events.get(eventChoice-1).getEventName() + " have the following availabilities:\n");
+		for (int i = 0; i < events.get(eventChoice-1).a_attendees.size(); i++)
+		{
+			String timestring = "\t";
+			System.out.println(events.get(eventChoice-1).a_attendees.get(i).getName() + ": ");
+			for (int j = 0; j < events.get(eventChoice-1).a_attendees.get(i).getAvailability().size(); j++)
+			{
+				if (timeChoice == 12)
+				{
+					timestring += " " + twelveHourConversion(events.get(eventChoice-1).a_attendees.get(i).getAvailability().get(j));
+				}
+				else if (timeChoice == 24)
+				{
+					timestring += " " + twentyFourHourConversion(events.get(eventChoice-1).a_attendees.get(i).getAvailability().get(j));
+				}
+			}
+			System.out.print(timestring + "\n");
+		}
+		
+	}
 	/**
 	 * This method will be called when the user selects join event in main.
 	 */
@@ -618,4 +687,419 @@ public class JoinEvent {
 			System.out.println("\n");
 		}
 	}
+	
+	private static void clearPrint(String text)
+	{
+		clearScreen();
+		System.out.println(text);
+	}
+	
+	private static void clearScreen()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			System.out.println("\n");
+		}
+	}
+	//method will convert the ints placed in the vectors to the needed strings for 12 hour mode
+	public static String twelveHourConversion(int i) 
+	{
+		String conversion = "\0";
+		
+		if(i==0)
+		{
+			conversion = "12:00AM";
+		}
+		if(i==1)
+		{
+			conversion = "12:30AM";
+		}
+		if(i==2)
+		{
+			conversion = "1:00AM";
+		}
+		if(i==3)
+		{
+			conversion = "1:30AM";
+		}
+		if(i==4)
+		{
+			conversion = "2:00AM";
+		}
+		if(i==5)
+		{
+			conversion = "2:30AM";
+		}
+		if(i==6)
+		{
+			conversion = "3:00AM";
+		}
+		if(i==7)
+		{
+			conversion = "3:30AM";
+		}
+		if(i==8)
+		{
+			conversion = "4:00AM";
+		}
+		if(i==9)
+		{
+			conversion = "4:30AM";
+		}
+		if(i==10)
+		{
+			conversion = "5:00AM";
+		}
+		if(i==11)
+		{
+			conversion = "5:30AM";
+		}
+		if(i==12)
+		{
+			conversion = "6:00AM";
+		}
+		if(i==13)
+		{
+			conversion = "6:30AM";
+		}
+		if(i==14)
+		{
+			conversion = "7:00AM";
+		}
+		if(i==15)
+		{
+			conversion = "7:30AM";
+		}
+		if(i==16)
+		{
+			conversion = "8:00AM";
+		}
+		if(i==17)
+		{
+			conversion = "8:30AM";
+		}
+		if(i==18)
+		{
+			conversion = "9:00AM";
+		}
+		if(i==19)
+		{
+			conversion = "9:30AM";
+		}
+		if(i==20)
+		{
+			conversion = "10:00AM";
+		}
+		if(i==21)
+		{
+			conversion = "10:30AM";
+		}
+		if(i==22)
+		{
+			conversion = "11:00AM";
+		}
+		if(i==23)
+		{
+			conversion = "11:30AM";
+		}
+		if(i==24)
+		{
+			conversion = "12:00PM";
+		}
+		if(i==25)
+		{
+			conversion = "12:30PM";
+		}
+		if(i==26)
+		{
+			conversion = "1:00PM";
+		}
+		if(i==27)
+		{
+			conversion = "1:30PM";
+		}
+		if(i==28)
+		{
+			conversion = "2:00PM";
+		}
+		if(i==29)
+		{
+			conversion = "2:30PM";
+		}
+		if(i==30)
+		{
+			conversion = "3:00PM";
+		}
+		if(i==31)
+		{
+			conversion = "3:30PM";
+		}
+		if(i==32)
+		{
+			conversion = "4:00PM";
+		}
+		if(i==33)
+		{
+			conversion = "4:30PM";
+		}
+		if(i==34)
+		{
+			conversion = "5:00PM";
+		}
+		if(i==35)
+		{
+			conversion = "5:30PM";
+		}
+		if(i==36)
+		{
+			conversion = "6:00PM";
+		}
+		if(i==37)
+		{
+			conversion = "6:30PM";
+		}
+		if(i==38)
+		{
+			conversion = "7:00PM";
+		}
+		if(i==39)
+		{
+			conversion = "7:30PM";
+		}
+		if(i==40)
+		{
+			conversion = "8:00PM";
+		}
+		if(i==41)
+		{
+			conversion = "8:30PM";
+		}
+		if(i==42)
+		{
+			conversion = "9:00PM";
+		}
+		if(i==43)
+		{
+			conversion = "9:30PM";
+		}
+		if(i==44)
+		{
+			conversion = "10:00PM";
+		}
+		if(i==45)
+		{
+			conversion = "10:30PM";
+		}
+		if(i==46)
+		{
+			conversion = "11:00PM";
+		}
+		if(i==47)
+		{
+			conversion = "11:30PM";
+		}
+		return conversion;
+	}
+	
+	//method converts ints in vectors into corresponding strings for 24:00 hour mode
+	public static String twentyFourHourConversion(int i) 
+	{
+		String conversion = "\0";
+		
+		if(i==0)
+		{
+			conversion = "0:00";
+		}
+		if(i==1)
+		{
+			conversion = "0:30";
+		}
+		if(i==2)
+		{
+			conversion = "1:00";
+		}
+		if(i==3)
+		{
+			conversion = "1:30";
+		}
+		if(i==4)
+		{
+			conversion = "2:00";
+		}
+		if(i==5)
+		{
+			conversion = "2:30";
+		}
+		if(i==6)
+		{
+			conversion = "3:00";
+		}
+		if(i==7)
+		{
+			conversion = "3:30";
+		}
+		if(i==8)
+		{
+			conversion = "4:00";
+		}
+		if(i==9)
+		{
+			conversion = "4:30";
+		}
+		if(i==10)
+		{
+			conversion = "5:00";
+		}
+		if(i==11)
+		{
+			conversion = "5:30";
+		}
+		if(i==12)
+		{
+			conversion = "6:00";
+		}
+		if(i==13)
+		{
+			conversion = "6:30";
+		}
+		if(i==14)
+		{
+			conversion = "7:00";
+		}
+		if(i==15)
+		{
+			conversion = "7:30";
+		}
+		if(i==16)
+		{
+			conversion = "8:00";
+		}
+		if(i==17)
+		{
+			conversion = "8:30";
+		}
+		if(i==18)
+		{
+			conversion = "9:00";
+		}
+		if(i==19)
+		{
+			conversion = "9:30";
+		}
+		if(i==20)
+		{
+			conversion = "10:00";
+		}
+		if(i==21)
+		{
+			conversion = "10:30";
+		}
+		if(i==22)
+		{
+			conversion = "11:00";
+		}
+		if(i==23)
+		{
+			conversion = "11:30";
+		}
+		if(i==24)
+		{
+			conversion = "12:00";
+		}
+		if(i==25)
+		{
+			conversion = "12:30";
+		}
+		if(i==26)
+		{
+			conversion = "13:00";
+		}
+		if(i==27)
+		{
+			conversion = "13:30";
+		}
+		if(i==28)
+		{
+			conversion = "14:00";
+		}
+		if(i==29)
+		{
+			conversion = "14:30";
+		}
+		if(i==30)
+		{
+			conversion = "15:00";
+		}
+		if(i==31)
+		{
+			conversion = "15:30";
+		}
+		if(i==32)
+		{
+			conversion = "16:00";
+		}
+		if(i==33)
+		{
+			conversion = "16:30";
+		}
+		if(i==34)
+		{
+			conversion = "17:00";
+		}
+		if(i==35)
+		{
+			conversion = "17:30";
+		}
+		if(i==36)
+		{
+			conversion = "18:00";
+		}
+		if(i==37)
+		{
+			conversion = "18:30";
+		}
+		if(i==38)
+		{
+			conversion = "19:00";
+		}
+		if(i==39)
+		{
+			conversion = "19:30";
+		}
+		if(i==40)
+		{
+			conversion = "20:00";
+		}
+		if(i==41)
+		{
+			conversion = "20:30";
+		}
+		if(i==42)
+		{
+			conversion = "21:00";
+		}
+		if(i==43)
+		{
+			conversion = "21:30";
+		}
+		if(i==44)
+		{
+			conversion = "22:00";
+		}
+		if(i==45)
+		{
+			conversion = "22:30";
+		}
+		if(i==46)
+		{
+			conversion = "23:00";
+		}
+		if(i==47)
+		{
+			conversion = "23:30";
+		}
+		return conversion;
+	}
+}
+
 }
